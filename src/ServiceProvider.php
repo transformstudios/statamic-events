da9277305db25d39fb071e05a1f6c137c96dff6e
edalzell <erin@thedalzells.org>
2020-09-17 14:12:16 -0700

    config is handled automatically now

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace TransformStudios\Events;
 
-use TransformStudios\Events\Tags\Events;
 use Statamic\Providers\AddonServiceProvider;
+use TransformStudios\Events\Tags\Events;
 
 class ServiceProvider extends AddonServiceProvider
 {
@@ -18,14 +18,5 @@ class ServiceProvider extends AddonServiceProvider
     public function boot()
     {
         parent::boot();
-
-        $this->bootConfig();
-    }
-
-    private function bootConfig()
-    {
-        $this->publishes([
-            __DIR__.'../config/events.php' => config_path('events.php'),
-        ]);
     }
 }
