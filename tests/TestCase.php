73ac98e5f93861f8ab18dba07f66a1f6d4f26712
edalzell <erin@thedalzells.org>
2021-02-25 11:44:09 -0800

    try to get tests to run

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -50,15 +50,10 @@ abstract class TestCase extends OrchestraTestCase
     {
         parent::resolveApplicationConfiguration($app);
 
-        $configs = [
-            'assets', 'cp', 'forms', 'static_caching',
-            'sites', 'stache', 'system', 'users',
-        ];
+        $configs = ['assets', 'cp', 'forms', 'routes', 'static_caching', 'sites', 'stache', 'system', 'users'];
 
         foreach ($configs as $config) {
-            $app['config']->set("statamic.$config", require(__DIR__."/../vendor/statamic/cms/config/{$config}.php"));
+            $app['config']->set("statamic.$config", require __DIR__."/../vendor/statamic/cms/config/{$config}.php");
         }
-
-        $app['config']->set('statamic.users.repository', 'file');
     }
 }
