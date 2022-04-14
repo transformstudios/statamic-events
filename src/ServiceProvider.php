2ac5dfa122e5425d9e9c1830bf2db17bfd658734
edalzell <erin@silentz.co>
2022-04-13 20:53:11 -0700

    set the week start/end based on the current site

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace TransformStudios\Events;
 
+use Illuminate\Support\Carbon;
+use Statamic\Facades\Site;
 use Statamic\Providers\AddonServiceProvider;
 use TransformStudios\Events\Modifiers\InMonth;
 use TransformStudios\Events\Modifiers\IsEndOfWeek;
@@ -31,5 +33,11 @@ class ServiceProvider extends AddonServiceProvider
         $this->publishes([
             __DIR__.'/../resources/fieldsets' => resource_path('fieldsets'),
         ], 'events-fieldsets');
+
+        // set weekstart/end
+        $weekStartDay = Carbon::getTranslator()->trans(id: 'first_day_of_week', locale: Site::current()->locale());
+
+        Carbon::setWeekStartsAt($weekStartDay);
+        Carbon::setWeekEndsAt(($weekStartDay + 6) % 7);
     }
 }
