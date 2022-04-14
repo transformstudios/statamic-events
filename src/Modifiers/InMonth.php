7713e747f62448dfb5881a73f0aeeaa7b01faed9
edalzell <erin@silentz.co>
2022-04-13 21:09:09 -0700

    tidy

--- src/Modifiers/InMonth.php
+++ src/Modifiers/InMonth.php
@@ -10,12 +10,12 @@ class InMonth extends Modifier
 {
     public function index($value, $params, $context)
     {
-        $date = Carbon::parse(
+        $month = Carbon::parse(
             Arr::get($context, 'get.month', Carbon::now()->englishMonth).
             ' '.
             Arr::get($context, 'get.year', Carbon::now()->year)
-        );
+        )->month;
 
-        return Carbon::parse($value)->month == $date->month;
+        return Carbon::parse($value)->month == $month;
     }
 }
