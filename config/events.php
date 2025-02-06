850adc6903b2e97858cf33b120d6adde03561ec0
edalzell <erin@silentz.co>
2025-02-06 15:21:55 -0800

    wip

--- config/events.php
+++ config/events.php
@@ -2,5 +2,12 @@
 
 return [
     'collection' => 'events',
+
+    'collections' => [
+        'events' => [
+            'location_field' => 'location',
+        ],
+    ],
+
     'timezone' => config('app.timezone'),
 ];
