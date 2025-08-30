d6af929b2b43693b6209c1b43d117cc7ae446b33
Erin Dalzell <erin@silentz.co>
2025-08-29 17:00:12 -0700

    Convert to Pest (#131)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -20,9 +20,22 @@ abstract class TestCase extends AddonTestCase
 
     protected string $addonServiceProvider = ServiceProvider::class;
 
+    protected $fakeStacheDirectory = __DIR__.'/__fixtures__/dev-null';
+
+    protected $shouldFakeVersion = true;
+
     protected Collection $collection;
 
     protected Blueprint $blueprint;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (! file_exists($this->fakeStacheDirectory)) {
+            mkdir($this->fakeStacheDirectory, 0777, true);
+        }
+    }
 
     protected function getEnvironmentSetUp($app)
     {
