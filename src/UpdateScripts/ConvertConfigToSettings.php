a24f56008d5d162da71c75176bfad97edd342f03
edalzell <erin@silentz.co>
2025-10-24 11:47:38 -0700

    make  new settings, remove old config

--- src/UpdateScripts/ConvertConfigToSettings.php
+++ src/UpdateScripts/ConvertConfigToSettings.php
@@ -4,12 +4,16 @@ namespace TransformStudios\Events\UpdateScripts;
 
 use Illuminate\Support\Fluent;
 use Illuminate\Support\Str;
-use Statamic\Facades\Addon;
+use Statamic\Addons\Addon;
+use Statamic\Addons\Settings;
+use Statamic\Facades\Addon as AddonFacade;
 use Statamic\Support\Arr;
 use Statamic\UpdateScripts\UpdateScript;
 
 class ConvertConfigToSettings extends UpdateScript
 {
+    private Addon $addon;
+
     public function shouldUpdate($newVersion, $oldVersion)
     {
         return $this->isUpdatingTo('6.0');
@@ -17,10 +21,37 @@ class ConvertConfigToSettings extends UpdateScript
 
     public function update()
     {
-        $config = Fluent::make(config('events'));
+        $this->addon = AddonFacade::get('transformstudios/events');
+
+        if (is_null($settings = $this->settingsFromConfig())) {
+            return;
+        }
+
+        if (! $settings->save()) {
+            $this->console()->error('Failed to save events settings. Please check your logs for details.');
+
+            return;
+        }
+
+        $this->console()->info('Converted events config to settings.');
+
+        $this->removeConfig();
+    }
+
+    private function removeConfig(): void
+    {
+        if ($this->files->exists($configPath = config_path('events.php'))) {
+            $this->files->delete($configPath);
+            $this->console()->info('Removed old events config file.');
+        }
+    }
+
+    private function settingsFromConfig(): ?Settings
+    {
+        $config = Fluent::make($this->addon->config());
 
         if ($config->isEmpty()) {
-            return;
+            return null;
         }
 
         $collections = collect([$config->collection => null])
@@ -45,9 +76,8 @@ class ConvertConfigToSettings extends UpdateScript
 
         $timezone = $config->timezone;
 
-        Addon::get('transformstudios/events')
+        return $this->addon
             ->settings()
-            ->set(Arr::removeNullValues(compact('collections', 'timezone')))
-            ->save();
+            ->set(Arr::removeNullValues(compact('collections', 'timezone')));
     }
 }
