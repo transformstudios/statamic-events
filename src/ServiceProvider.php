fb99cdceca9cb8563b4dbea284107305bffc6e3e
edalzell <erin@silentz.co>
2026-02-06 12:58:10 -0800

    simplify

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,17 +2,12 @@
 
 namespace TransformStudios\Events;
 
-use Carbon\Carbon;
-use Carbon\CarbonImmutable;
-use Illuminate\Support\Facades\Artisan;
 use Statamic\Entries\Entry;
 use Statamic\Facades\Collection;
-use Statamic\Facades\Site;
 use Statamic\Fields\Field;
 use Statamic\Fields\Value;
 use Statamic\Fieldtypes\Dictionary;
 use Statamic\Providers\AddonServiceProvider;
-use Statamic\Statamic;
 
 class ServiceProvider extends AddonServiceProvider
 {
@@ -22,41 +17,12 @@ class ServiceProvider extends AddonServiceProvider
 
     public function bootAddon()
     {
-        $this
-            ->bootCarbon()
-            ->bootFields()
-            ->publishConfig();
-    }
-
-    private function bootCarbon(): self
-    {
-        Carbon::setLocale(Site::current()->locale());
-        CarbonImmutable::setLocale(Site::current()->locale());
-
-        return $this;
-    }
-
-    private function bootFields(): self
-    {
         collect(Events::setting('collections', [['collection' => 'events']]))
-            ->each(fn (array $collection) => $this
-                ->defineComputedTimezoneField($collection['collection']));
-
-        return $this;
-    }
-
-    private function defineComputedTimezoneField(string $handle): void
-    {
-        Collection::computed($handle, 'timezone', $this->timezone(...));
-    }
-
-    private function publishConfig(): self
-    {
-        Statamic::afterInstalled(function ($command) {
-            Artisan::call('vendor:publish', ['--tag' => 'events-config', '--force' => false]);
-        });
-
-        return $this;
+            ->each(fn (array $collection) => Collection::computed(
+                $collection['collection'],
+                'timezone',
+                $this->timezone(...)
+            ));
     }
 
     private function timezone(Entry $entry, $value): string|Value
