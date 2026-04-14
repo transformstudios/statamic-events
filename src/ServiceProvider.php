988232dbe8aa3fb77c309b7320b9666d30707123
edalzell <erin@silentz.co>
2026-04-14 12:50:10 -0700

    addon’s booting callback

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,12 +2,8 @@
 
 namespace TransformStudios\Events;
 
-use Statamic\Entries\Entry;
 use Statamic\Facades\Collection;
 use Statamic\Facades\Field as FieldFacade;
-use Statamic\Fields\Field;
-use Statamic\Fields\Value;
-use Statamic\Fieldtypes\Dictionary;
 use Statamic\Providers\AddonServiceProvider;
 use Statamic\Statamic;
 
@@ -18,26 +14,19 @@ class ServiceProvider extends AddonServiceProvider
         FieldFacade::computedDefault('default-events-timezone', fn () => Statamic::displayTimezone());
         FieldFacade::computedDefault('default-event-timezone', fn () => Events::defaultTimezone());
 
-        collect(Events::setting('collections', ['events']))
-            ->each(fn (string $collection) => Collection::computed(
-                $collection,
-                'timezone',
-                $this->timezone(...)
-            ));
-    }
-
-    private function timezone(Entry $entry, $value): string|Value
-    {
-        $value ??= Events::defaultTimezone();
-
-        if ($entry->blueprint()->fields()->get('timezone')?->fieldtype() instanceof Dictionary) {
-            return $value;
-        }
-
-        return (new Field('timezone', ['dictionary' => 'timezones', 'max_items' => '1', 'type' => 'dictionary']))
-            ->setValue($value)
-            ->setParent($entry)
-            ->augment()
-            ->value();
+        // has to be in booted so that the `events::event` fieldset is properly registered and loaded
+        $this->booted(function () {
+            collect(Events::setting('collections', ['events']))
+                ->each(function (string $collection) {
+                    Collection::findByHandle($collection)?->entryBlueprint()->ensureField(
+                        'timezone',
+                        [
+                            'dictionary' => 'timezones',
+                            'max_items' => '1',
+                            'type' => 'dictionary',
+                            'default' => 'computed:default-event-timezone',
+                        ]);
+                });
+        });
     }
 }
